1b1ccadc6b0bf82eb80db8bf3943e2a5a3f59ed1
jknox927 <joshua.knox927@gmail.com>
2022-12-20 03:47:31 +0000

    init commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector("button").addEventListener("click", clickMe);
+
+function generateDomain() {
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+
+  let arr = [];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        arr.push(pronoun + adj + noun + ".com");
+      }
+    }
+  }
+  return arr;
+}
+
+for (let url of generateDomain()) {
+  document.getElementById("result").innerHTML += "<br>" + url;
+}
+
+function clickMe() {
+  alert("But for now, rest well and dream of large errors");
+}
